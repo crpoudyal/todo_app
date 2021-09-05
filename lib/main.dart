6484b4eb48101390591c6466b06ffca6f196cceb
crpoudyal <crpoudyal@gmail.com>
2021-09-05 20:05:53 +0545

    main file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+void main() {
+  runApp(Todo());
+}
+
+class Todo extends StatelessWidget {
+  const Todo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+    );
+  }
+}
